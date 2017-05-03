a3241fce036c7ac9097d857dec5f33f96dd72eee
Adara <adarahv@gmail.com>
2017-05-03 22:46:33 +0300

    truly initial commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const { Composer, Markup } = require('micro-bot')
+const msg = require('./messages')
 
 const app = new Composer()
 //app.set('port', (process.env.PORT || 5000))
 
 app.command('start', (ctx) =>
-  ctx.reply('42')
-  // ctx.replyWithMarkdown(msg.start, Markup
-  //   .keyboard([['так', 'ишо']])
-  //   .resize()
-  //   .extra()
-  // )
+  ctx.replyWithMarkdown(msg.start, Markup
+    .keyboard(myKeyboard)
+    .resize()
+    .extra()
+  )
 )
+const myKeyboard = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['0', ':']
+];
+
 app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+app.hears(/\d\d:\d\d/, (ctx) => ctx.reply('time setted! and one more question about location'))
 
-
+// I need your city 
+// then Sorry it's invalid'
+// Then - Deal!`
+// help use command 'set time' or change location/ pause / exercise
+  // ctx.reply('42')
 // app.command('help', ctx => ctx.replyWithMarkdown(msg.help))
 // app.hears(/(так)|(ишо)/i, ctx => ctx.replyWithMarkdown(msg.formatQuote(getQuote({ details: true }))))
+
+// with inlintKeybord done or postopne for 15,30,45,60 (as skeddy buttons) or fck it
 
 module.exports = app
---
+++ messages.js
@@ -0,0 +1,8 @@
+
+const start = `
+Hi! 
+What time do you prefer for eye exercises? 
+Use format e.g. 15:30`
+
+
+module.exports = { start }
