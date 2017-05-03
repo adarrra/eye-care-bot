ae44837e62ea56963f0ac91f06fabbac5db3beda
Adara <adarahv@gmail.com>
2017-05-03 20:10:52 +0300

    some mess

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = async function (ctx) {
-  await ctx.reply('42')
-}
+const { Composer, Markup } = require('micro-bot')
+
+const app = new Composer()
+//app.set('port', (process.env.PORT || 5000))
+
+app.command('start', (ctx) =>
+  ctx.reply('42')
+  // ctx.replyWithMarkdown(msg.start, Markup
+  //   .keyboard([['так', 'ишо']])
+  //   .resize()
+  //   .extra()
+  // )
+)
+app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+
+
+// app.command('help', ctx => ctx.replyWithMarkdown(msg.help))
+// app.hears(/(так)|(ишо)/i, ctx => ctx.replyWithMarkdown(msg.formatQuote(getQuote({ details: true }))))
+
+module.exports = app
