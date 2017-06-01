70032598c3a2624d94fc432e665a112dc03833ce
adarrra <adarahv@gmail.com>
2017-06-01 16:20:36 +0300

    fix moment

--- schedule_checker.js
+++ schedule_checker.js
@@ -1,7 +1,7 @@
 //every ten minutes check - are some notifications in next 10 min
 // playground: https://repl.it/I1Hz/2
 
-const pinger = require('express-ping');
+const requestify = require('requestify');
 const express = require('express');
 const dbUrl = process.env.MONGODB_URI;
 const monk = require('monk');
@@ -22,15 +22,12 @@ db.then(() => {
 
 });
 
-function isInNext10Min(time, tz, close) {
+function isInNext10Min(time, timezone, close) {
     // moment.tz.setDefault("UTC");
-    console.log(time);
-    let now = moment.utc();
-    console.log('now utc:', now);
-    let notif = moment.utc(moment.tz(time, 'hh:mm', tz));
-    console.log('from db utc: ', notif);
-    let next10min = now.add(10, 'm');
-    console.log('next10', next10min);
+    console.log(time, timezone);
+    const now = moment.utc();
+    const notif = moment.utc(moment.tz(time, 'hh:mm', timezone));
+    const next10min = moment.utc().add(10, 'm');
     console.log('is btw: ', now.isBetween(now, next10min));
     console.log('is before +10: ', notif.isBefore(next10min));
     console.log('is after now: ', notif.isAfter(now.format()));
@@ -42,6 +39,7 @@ function isInNext10Min(time, tz, close) {
 
 function pingToWakeUp() {
     console.log('i am pinger');
-    const app = express();
-    app.use(pinger.ping());
+    requestify.get(process.env.URL).then(function(response) {
+        console.log(response.getBody());
+    });
 }
