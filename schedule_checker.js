ccd052b15dcebb9d8532c9ac8cb6aae93ef81a2a
adarrra <adarahv@gmail.com>
2017-06-01 17:11:11 +0300

    fix function order in scheduler

--- schedule_checker.js
+++ schedule_checker.js
@@ -12,29 +12,30 @@ const moment = require('moment-timezone');
 db.then(() => {
     console.log('schedule checker connected with db');
     users.find({}).each((user, {close, pause, resume}) => {
-        user.notifications.forEach(time => {
-            isInNext10Min(time.full, user.timezone, close)
+        let found = user.notifications.some(time => {
+            return isInNext10Min(time.full, user.timezone, close)
         });
-    }).then(() => {
-        console.log('we go to exit');
-        // process.exit()
+        if (!found) {
+            console.log('not found, we go to exit');
+            process.exit();
+        }
+    }).catch(() => {
+        console.log('error, we go to exit');
+        process.exit()
     });
 
 });
 
 function isInNext10Min(time, timezone, close) {
-    // moment.tz.setDefault("UTC");
     console.log(time, timezone);
     const now = moment.utc();
     const notif = moment.utc(moment.tz(time, 'hh:mm', timezone));
     const next10min = moment.utc().add(10, 'm');
-    // console.log('is btw: ', now.isBetween(now, next10min));
-    // console.log('is before +10: ', notif.isBefore(next10min));
-    // console.log('is after now: ', notif.isAfter(now.format()));
     if (notif.isBetween(now, next10min)) {
         console.log('I found - call pinger');
-        pingToWakeUp()
         close()
+        pingToWakeUp()
+        return true;
     }
 }
 
@@ -42,5 +43,6 @@ function pingToWakeUp() {
     console.log('i am pinger');
     requestify.get(process.env.URL).then(function(response) {
         console.log('resp: ', response.getBody());
+        process.exit();
     });
 }
